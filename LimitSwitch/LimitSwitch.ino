f3bbcf367dea27209de40dfb445fdf50db065f0e
Stacy000 <sxy20000401@gmail.com>
2023-07-19 21:51:05 -0700

    Update LimitSwitch.ino

--- LimitSwitch/LimitSwitch.ino
+++ LimitSwitch/LimitSwitch.ino
@@ -1,11 +1,16 @@
-
+#include <Stepper.h>
 #define IN1 8
 #define IN2 9
 #define IN3 10
 #define IN4 11
+#define ENA A8
+#define ENB A9
 
 #define SW1 15
 #define SW2 18
+
+const int stepsPerRevolution = 500;
+Stepper myStepper = Stepper(stepsPerRevolution, 8,9,10,11);
 
 bool motorStop = false;
 volatile unsigned long lastInterruptTime = 0;
@@ -28,16 +33,50 @@ void setup() {
   digitalWrite(SW1, HIGH);
   
   Serial.begin(4800);
+
+  myStepper.setSpeed(10);
     
   motorReset();
 }
 
 
 void loop() {
+
+// Step one revolution in one direction:
+  myStepper.step(500);
+
+  //delay(2000);
+
+  // Step on revolution in the other direction:
+  myStepper.step(-500);
+  // digitalWrite(IN1, HIGH);
+  // digitalWrite(IN2, LOW);
+  // digitalWrite(IN3, LOW);
+  // digitalWrite(IN4, HIGH);
+
+  // // analogWrite(ENA, 50);
+  // // analogWrite(ENB, 50);
+
+
+  // delay(1000);
+
+  // digitalWrite(IN1, LOW);
+  // digitalWrite(IN2, HIGH);
+  // digitalWrite(IN3, HIGH);
+  // digitalWrite(IN4, LOW);
+
+  // // analogWrite(ENA, 50);
+  // // analogWrite(ENB, 50);
   
-  Serial.print("Nothing");
-  Serial.write("\n");
-  delay(1000);
+  // // for(int i=0; i<1200; i++) {
+  // //   digitalWrite(pul, HIGH);
+  // //   delay(1);
+  // //   digitalWrite(pul, LOW);
+  // //   delay(1);
+  // //   }
+  // Serial.print("Nothing");
+  // Serial.write("\n");
+  // // delay(1000);
 }
 
 void stopMotor(){
