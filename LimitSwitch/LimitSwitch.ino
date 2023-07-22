535067478f1014a359e5d58f5a051d51b68b4476
Stacy000 <sxy20000401@gmail.com>
2023-07-22 14:13:18 -0700

    Update LimitSwitch.ino

--- LimitSwitch/LimitSwitch.ino
+++ LimitSwitch/LimitSwitch.ino
@@ -1,122 +1,83 @@
-#include <Stepper.h>
-#define IN1 8
-#define IN2 9
-#define IN3 10
-#define IN4 11
-#define ENA A8
-#define ENB A9
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 
-#define SW1 15
-#define SW2 18
+#define LS1 15
+#define LS2 18
 
-const int stepsPerRevolution = 500;
-Stepper myStepper = Stepper(stepsPerRevolution, 8,9,10,11);
+const int stepsPerRevolution = 10;
 
-bool motorStop = false;
+// Define a 4 wire stepper motor to pin 8 9 10 11
+AccelStepper spoolStepper(4,8,9,10,11);
+
+// Define variables
+bool motorBackward = false;
+bool motorHoming = true;
+bool spooling = false;
 volatile unsigned long lastInterruptTime = 0;
 const unsigned long interruptInterval = 500;
 
+
 void setup() {
-  pinMode(IN1, OUTPUT);
-  
-  pinMode(IN2, OUTPUT);
+  // Set one of the limit switch signal to high
+  pinMode(LS1, OUTPUT);
+  digitalWrite(LS1, HIGH);
 
-  pinMode(IN3, OUTPUT);
-
-  pinMode(IN4, OUTPUT);
+  // Set the other limit switch signal as input; the pin is at high state by default
+  pinMode(LS2, INPUT_PULLUP);
   
-  pinMode(SW1, OUTPUT);
-  pinMode(SW2, INPUT_PULLUP);
-  
-  attachInterrupt(digitalPinToInterrupt(SW2), stopMotor, FALLING);
+  // An interrupt that triggers when the limit switch is pressed
+  attachInterrupt(digitalPinToInterrupt(LS2), stopMotor, FALLING);
 
-  digitalWrite(SW1, HIGH);
-  
-  Serial.begin(4800);
+  // Set the maximum speed to 150
+  spoolStepper.setMaxSpeed(150);
 
-  myStepper.setSpeed(10);
-    
-  motorReset();
+  // Set the current motor position to 0
+  spoolStepper.setCurrentPosition(0);
 }
 
 
 void loop() {
-
-// Step one revolution in one direction:
-  myStepper.step(500);
-
-  //delay(2000);
-
-  // Step on revolution in the other direction:
-  myStepper.step(-500);
-  // digitalWrite(IN1, HIGH);
-  // digitalWrite(IN2, LOW);
-  // digitalWrite(IN3, LOW);
-  // digitalWrite(IN4, HIGH);
-
-  // // analogWrite(ENA, 50);
-  // // analogWrite(ENB, 50);
-
-
-  // delay(1000);
-
-  // digitalWrite(IN1, LOW);
-  // digitalWrite(IN2, HIGH);
-  // digitalWrite(IN3, HIGH);
-  // digitalWrite(IN4, LOW);
-
-  // // analogWrite(ENA, 50);
-  // // analogWrite(ENB, 50);
   
-  // // for(int i=0; i<1200; i++) {
-  // //   digitalWrite(pul, HIGH);
-  // //   delay(1);
-  // //   digitalWrite(pul, LOW);
-  // //   delay(1);
-  // //   }
-  // Serial.print("Nothing");
-  // Serial.write("\n");
-  // // delay(1000);
+  if(motorHoming == true)
+  {  
+    spoolStepper.setAcceleration(1200);
+    spoolStepper.moveTo(-5000);
+    spoolStepper.runToPosition();
+  }
+
+  if(motorBackward == true)
+  {
+    spoolStepper.setCurrentPosition(0);
+    spoolStepper.setAcceleration(70);
+    spoolStepper.moveTo(1300);
+    spoolStepper.runToPosition();
+    motorBackward = false;
+    spooling = true;
+  }
+
+  if(spooling == true)
+  {
+    spoolStepper.setCurrentPosition(0);
+    spoolStepper.setMaxSpeed(20);
+    spoolStepper.setAcceleration(1000);
+    spoolStepper.moveTo(1000);
+    //spoolStepper.setSpeed(50);
+    spoolStepper.runToPosition();
+
+    spoolStepper.moveTo(0);
+    spoolStepper.runToPosition();
+  }
 }
 
 void stopMotor(){
   unsigned long currentMillis = millis();
   if (currentMillis - lastInterruptTime >= interruptInterval) {
-    motorStop=true;
-    Serial.print("Switch pressed");
-    Serial.write("\n");
+    spoolStepper.stop();
+    //Serial.print("Switch pressed");
+    motorBackward = true;
+    motorHoming = false;
+    //Serial.write("\n");
     // Update the last interrupt time
     lastInterruptTime = currentMillis;
   }
-}
-
-void motorReset(){
-  // Serial.print("Start homing");
-  // Serial.write("\n");
-  
-  
-  
-  // Serial.print("Homing");
-  // Serial.write("\n");
-  // digitalWrite(dir, LOW);// Set the homing direction
-  // // Keep rotating till the switch is pressed
-  // while(motorStop==false){
-  //   digitalWrite(pul, HIGH);
-  //   delay(1);
-  //   digitalWrite(pul, LOW);
-  //   delay(1);
-  // }
-
-  // Serial.print("Adjusting position");
-  // Serial.write("\n");
-  // digitalWrite(dir, HIGH);
-  // for(int i=0; i<1000; i++) {
-  //   digitalWrite(pul, HIGH);
-  //   delay(1);
-  //   digitalWrite(pul, LOW);
-  //   delay(1);
-
-  //   }
-  // Serial.print("Homing finished");
-  // Serial.write("\n");
 }
